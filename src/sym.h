7262c2709b4dcf2dc121d89ce00ca57f3b47c8d8
mciiee <mrchingiz91@gmail.com>
2025-12-03 16:51:53 +0500

    feat: moved all symbol definitions to a separate header

---
+++ src/sym.h
@@ -0,0 +1,55 @@
+#ifndef SYM_H
+#define SYM_H
+constexpr char SYM_ALPHA_LOWERCASE[]      = "\u03B1";
+constexpr char SYM_BETA_LOWERCASE[]       = "\u03B2";
+constexpr char SYM_GAMMA_LOWERCASE[]      = "\u03B3";
+constexpr char SYM_DELTA_LOWERCASE[]      = "\u03B4";
+constexpr char SYM_EPSILON_LOWERCASE[]    = "\u03B5";
+constexpr char SYM_ZETA_LOWERCASE[]       = "\u03B6";
+constexpr char SYM_ETA_LOWERCASE[]        = "\u03B7";
+constexpr char SYM_THETA_LOWERCASE[]      = "\u03B8";
+constexpr char SYM_IOTA_LOWERCASE[]       = "\u03B9";
+constexpr char SYM_KAPPA_LOWERCASE[]      = "\u03BA";
+constexpr char SYM_LAMBDA_LOWERCASE[]     = "\u03BB";
+constexpr char SYM_MU_LOWERCASE[]         = "\u03BC";
+constexpr char SYM_NU_LOWERCASE[]         = "\u03BD";
+constexpr char SYM_XI_LOWERCASE[]         = "\u03BE";
+constexpr char SYM_OMICRON_LOWERCASE[]    = "\u03BF";
+constexpr char SYM_PI_LOWERCASE[]         = "\u03C0";
+constexpr char SYM_RHO_LOWERCASE[]        = "\u03C1";
+constexpr char SYM_SIGMA_FINAL[]          = "\u03C2";
+constexpr char SYM_SIGMA_LOWERCASE[]      = "\u03C3";
+constexpr char SYM_TAU_LOWERCASE[]        = "\u03C4";
+constexpr char SYM_UPSILON_LOWERCASE[]    = "\u03C5";
+constexpr char SYM_PHI_LOWERCASE[]        = "\u03C6";
+constexpr char SYM_CHI_LOWERCASE[]        = "\u03C7";
+constexpr char SYM_PSI_LOWERCASE[]        = "\u03C8";
+constexpr char SYM_OMEGA_LOWERCASE[]      = "\u03C9";
+
+
+constexpr char SYM_ALPHA_UPPERCASE[]      = "\u0391";
+constexpr char SYM_BETA_UPPERCASE[]       = "\u0392";
+constexpr char SYM_GAMMA_UPPERCASE[]      = "\u0393";
+constexpr char SYM_DELTA_UPPERCASE[]      = "\u0394";
+constexpr char SYM_EPSILON_UPPERCASE[]    = "\u0395";
+constexpr char SYM_ZETA_UPPERCASE[]       = "\u0396";
+constexpr char SYM_ETA_UPPERCASE[]        = "\u0397";
+constexpr char SYM_THETA_UPPERCASE[]      = "\u0398";
+constexpr char SYM_IOTA_UPPERCASE[]       = "\u0399";
+constexpr char SYM_KAPPA_UPPERCASE[]      = "\u039A";
+constexpr char SYM_LAMBDA_UPPERCASE[]     = "\u039B";
+constexpr char SYM_MU_UPPERCASE[]         = "\u039C";
+constexpr char SYM_NU_UPPERCASE[]         = "\u039D";
+constexpr char SYM_XI_UPPERCASE[]         = "\u039E";
+constexpr char SYM_OMICRON_UPPERCASE[]    = "\u039F";
+constexpr char SYM_PI_UPPERCASE[]         = "\u03A0";
+constexpr char SYM_RHO_UPPERCASE[]        = "\u03A1";
+constexpr char SYM_SIGMA_UPPERCASE[]      = "\u03A3";
+constexpr char SYM_TAU_UPPERCASE[]        = "\u03A4";
+constexpr char SYM_UPSILON_UPPERCASE[]    = "\u03A5";
+constexpr char SYM_PHI_UPPERCASE[]        = "\u03A6";
+constexpr char SYM_CHI_UPPERCASE[]        = "\u03A7";
+constexpr char SYM_PSI_UPPERCASE[]        = "\u03A8";
+constexpr char SYM_OMEGA_UPPERCASE[]      = "\u03A9";
+
+#endif
