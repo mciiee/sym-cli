a8ac97b8d35d8c7829a53afab047e0abb29cb808
mciiee <mrchingiz91@gmail.com>
2026-06-27 19:25:20 +0500

    feat: added custom separators

--- src/sym-cli.c
+++ src/sym-cli.c
@@ -11,21 +11,36 @@ struct CliOpts opts = {
   .sep = "\n",
 };
 
-
+int parseCliArgs(int argc, char *argv[], struct CliOpts *opts) {
+  for (int c = getopt(argc, argv, "::s:"); c != -1; c = getopt(argc, argv, "::s:")) {
+    switch (c) {
+      case 's':
+        opts->sep = optarg;
+        break;
+      case '?':
+        LOG_ERROR("Unknown option: %c\n", c);
+        break;
+    }
+  }
+  return optind;
+}
 
 int main(int argc, char **argv) {
   if (argc <= 0) {
     LOG_ERROR("No symbols given");
     return -1;
   }
+
+  int start = parseCliArgs(argc, argv, &opts);
+
   const char *sym = nullptr;
-  for (size_t i = 1; i < (size_t)argc; i++) {
+  for (size_t i = (size_t)start; i < (size_t)argc; i++) {
     sym = lookupSymbol(argv[i]);
     if (sym == nullptr) {
       fprintf(stderr, ERROR_PREFIX "\"%s\" is not a valid symbol\n", argv[i]);
       continue;
     }
-    if (i != 1) {
+    if (i != (size_t)start) {
       fputs(opts.sep, stdout);
     }
     printf("%s", sym);
